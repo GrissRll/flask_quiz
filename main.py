e00ead8aeab5ac3e16ceed686ee9e8f989e5b792
Griss <grissrll@gmail.com>
2026-04-14 14:48:20 +0300

    CREATE ORUT FOR TEST
    	main.py:
    	update endpoint test
    	create endpoint resut
    
    	templates:
    	create template for test view
    	create style.css

--- main.py
+++ main.py
@@ -14,19 +14,40 @@ app.config.update(dict(DATABASE=os.path.join(app.root_path, "quiz_data.db")))
 db = DataBase(app.config.get("DATABASE"))
 
 
-@app.route("/", methods=["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     quiz = db.get_data(SL_ALL_QUIZ)
+    if not session.get("q_num"):
+        session["q_num"] = 1
+        session["user_answers"] = []
     if request.method == "POST":
         session["quiz_id"] = request.form.get("quiz_list")
         session["question_list"] = db.get_data(SL_QUESTION, session["quiz_id"])
         return redirect(url_for("test"))
     return render_template("index.html", quiz_list=quiz, title="ТОП ВИКТОРИНЫ!")
 
+
 @app.route("/test", methods=["GET", "POST"])
 def test():
-    print(session["question_list"])
-    return "test page"
+    if session["q_num"] == len(session["question_list"]):
+        return redirect(url_for("result"))
+
+    full_q = session["question_list"][session["q_num"]]
+    q_list = full_q[1:]
+    question = full_q[0]
+    if request.method == "POST":
+        session["q_num"] += 1
+        user_ans = request.form.get("answer")
+        session["user_answers"].append(user_ans)
+        return redirect(url_for("test"))
+
+    return render_template("test.html", q_list=q_list, question=question)
+
+
+@app.route("/result")
+def result():
+    return "results"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
